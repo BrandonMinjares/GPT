f7a1405726fe1975cbcc2f7e7f40ee6b88f3da83
Brandon Minjares <brandon.minjares@gmail.com>
2023-05-06 14:32:55 -0700

    changed max width of root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import {Route, HashRouter, Routes} from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
-import { ChakraProvider } from '@chakra-ui/react';
 import Room from './components/Room';
 
 function App() {
   return (
     <HashRouter>
-      <ChakraProvider>
       <Routes>
         <Route path="/" element={<Dashboard></Dashboard>}></Route>
         <Route path="/game" element={<Room></Room>}></Route>
       </Routes>
-      </ChakraProvider>
     </HashRouter>
   );
 }
