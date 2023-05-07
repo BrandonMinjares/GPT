6676c5b4ad510dae24b95f052427ca190a39d42c
Brandon Minjares <brandon.minjares@gmail.com>
2023-05-06 23:49:09 -0700

    modified chatgpt prompt to give one answer and displaying question and answer

--- frontend/src/components/Room.jsx
+++ frontend/src/components/Room.jsx
@@ -1,8 +1,12 @@
 import { Box, Button, GridItem, Grid } from '@chakra-ui/react'
 import { Image} from '@chakra-ui/react'
 import MovieLogo from './../assets/film-reel-svgrepo-com.svg';
+import { useState } from 'react';
 
 const Room = () => {
+  const [question, setQuestion] = useState('');
+  const [option, setOption] = useState('');
+  const [answer, setAnswer] = useState('');
 
 
 
@@ -16,8 +20,8 @@ const Room = () => {
             'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-            prompt: 'Give me 5 movie trivia questions and provide 3 incorrect answers and 1 correct answer. Label each answer as A, B, C, or D. Store all the questions in 1 array, all the answer options in another array, and all the correct answers in another array and return the 3 arrays',
-            max_tokens: 2048,
+            prompt: 'Provide me with 1 movie trivia question in an array named questions, 4 unique answer options labeled A, B, C, or D in an array named options, and then 1 correct answer labeled A, B, C, or D in an array named answers. Make each array name lowercase.  Each array should be using the notation "[]"',
+            max_tokens: 4000,
             model: 'text-davinci-003',
             n: 1,
           })
@@ -26,18 +30,30 @@ const Room = () => {
         await fetch(`${process.env.REACT_APP_OPENAI_API_URL}`, options)
                                 .then(response => response.json())
                                 .then(data => {
-                                    console.log(data);
-                                    const answersString = data.choices[0].text.trim();
+                                  console.log(data);
+                                  const result = data.choices[0].text;
+                                  console.log(result);
+                                  const regex = /\[(.*?)\]/g;
+                                  const matches = [...result.matchAll(regex)].map(match => match[1]);
+                                  console.log(matches);
+                                  const question = matches[0].replace(/"/g, '');
 
-                                    const answers = answersString.split("\n\n");
-                                    console.log(answers);
-                                    
-                                    /*
-                                    const [questionsArray, answerOptionsArray, correctAnswersArray] = data.choices[0].text.trim().split(/\n\n(.*?)(?=\[)/).slice(1);
-                                    console.log(questionsArray);
-                                    console.log(answerOptionsArray);
-                                    console.log(correctAnswersArray);
-                                    */
+                                  setQuestion(question)
+
+                                  const options = matches[1].replace(/"/g, '');
+                                  setOption(options)
+
+                                  const answer = matches[2].replace(/"/g, '');
+                                  console.log(answer)
+
+
+                                  const optionArray = options.split(', ')
+
+                                  for (let i = 0; i < optionArray.length; i++) {
+                                    if (optionArray[i][0] === answer) {
+                                      setAnswer(optionArray[i])
+                                    }
+                                  }
                                 })
                                 .catch(error => console.log(error));
         }
@@ -45,7 +61,9 @@ const Room = () => {
     <Box>
     <Box bg='tomato' p='2vh' color='white'>
     <Image src={MovieLogo} alt="React Logo" style={{width: 45 }}/>
+        {question}
         <Button color='black' onClick={createGame}>Start Game</Button>
+        {answer}
     </Box>
     <Grid h='200px' templateRows='repeat(2, 1fr)' templateColumns='repeat(2, 1fr)'>
         <GridItem colSpan={1} bg='purple.600'  height='50vh' width='50vw' border='1px' borderColor='gray.200'>A</GridItem>
